67b610984344d6bf7b122533eec1914850db0515
Alex Warlen <warlen17@up.edu>
2017-03-23 16:45:31 -0700

    Added google charts reference and connection to database. TODO: insert data into database and use charts to visualize it. Charts can currently only visualize hard-coded data

---
+++ getData.php
@@ -0,0 +1,18 @@
+<?php 
+    $con = mysqli_connect('egr4.campus.up.edu','warlen17','warlen17','warlen17')
+    or die('Error connecting to MySQL server.');
+    // This is just an example of reading server side data and sending it to the client.
+    // It reads a json formatted text file and outputs it.
+
+    $string = file_get_contents("sampleData.json");
+    echo $string;
+    
+    $query = "Select * from test";
+    echo $query;
+    
+    $exec = mysqli_query($con,$query);
+    echo $exec;
+
+// Instead you can query your database and parse into JSON etc etc
+
+?>
