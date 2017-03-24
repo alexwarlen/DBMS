83acd914ca672feea96b89ae9b5bc4ef48249e91
Alex Warlen <warlen17@up.edu>
2017-03-23 17:10:07 -0700

    Testing data retrieval

--- getData.php
+++ getData.php
@@ -4,7 +4,7 @@
     // This is just an example of reading server side data and sending it to the client.
     // It reads a json formatted text file and outputs it.
 
-    $string = file_get_contents("sampleData.json");
+    $string = "hello world";
     echo $string;
     
     /*$query = "Select * from test";
